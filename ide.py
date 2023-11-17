5c9a3737570b0a36806e1b5ac518d376d424de72
TalosTheMinoan <88402424+TalosTheMinoan@users.noreply.github.com>
2023-11-18 00:23:01 +0200

    Update ide.py

--- ide.py
+++ ide.py
@@ -1,5 +1,7 @@
 import tkinter as tk
-from tkinter import filedialog, scrolledtext, messagebox, ttk, simpledialog
+from tkinter import filedialog, scrolledtext
+import subprocess
+import webbrowser
 
 class SimpleIDE:
     def __init__(self, root):
@@ -13,23 +15,16 @@ class SimpleIDE:
         - Replaced tk.Text with tk.scrolledtext.ScrolledText for better navigation.
         - Added Help menu with a link to Python documentation.
         - Added a Run Code menu to execute Python code.
-        - Added Clear Console menu to clear the console.
-        - Improved the GUI with better styling.
-        - Implemented user settings for font size.
-        - Added the ability to open new code tabs.
         """
-        self.show_update_log_notification()
+        self.show_update_log_popup()
 
-        # User Settings
-        self.font_size = tk.StringVar()
-        self.font_size.set("12")
+        self.text = scrolledtext.ScrolledText(self.root, wrap="word", undo=True)
+        self.text.pack(expand="yes", fill="both")
 
-        # Tabs
-        self.notebook = ttk.Notebook(self.root)
-        self.notebook.pack(expand="yes", fill="both")
-
-        # Initial code tab
-        self.add_code_tab()
+        # Console
+        self.console = scrolledtext.ScrolledText(self.root, wrap="word", height=10, state=tk.DISABLED)
+        self.console.pack(expand="yes", fill="both")
+        self.console.insert(tk.END, "Python Console\n")
 
         # Menu Bar
         menu_bar = tk.Menu(self.root)
@@ -38,7 +33,7 @@ class SimpleIDE:
         # File Menu
         file_menu = tk.Menu(menu_bar, tearoff=0)
         menu_bar.add_cascade(label="File", menu=file_menu)
-        file_menu.add_command(label="New", command=self.add_code_tab)
+        file_menu.add_command(label="New", command=self.new_file)
         file_menu.add_command(label="Open", command=self.open_file)
         file_menu.add_command(label="Save", command=self.save_file)
         file_menu.add_command(label="Save As", command=self.save_as_file)
@@ -50,39 +45,13 @@ class SimpleIDE:
         menu_bar.add_cascade(label="Run", menu=run_menu)
         run_menu.add_command(label="Run Code", command=self.run_code)
 
-        # Edit Menu
-        edit_menu = tk.Menu(menu_bar, tearoff=0)
-        menu_bar.add_cascade(label="Edit", menu=edit_menu)
-        edit_menu.add_command(label="Undo", command=self.undo)
-        edit_menu.add_command(label="Redo", command=self.redo)
-
-        # Console Menu
-        console_menu = tk.Menu(menu_bar, tearoff=0)
-        menu_bar.add_cascade(label="Console", menu=console_menu)
-        console_menu.add_command(label="Clear Console", command=self.clear_console)
-        console_menu.add_checkbutton(label="Show Console", command=self.toggle_console)
-
-        # Settings Menu
-        settings_menu = tk.Menu(menu_bar, tearoff=0)
-        menu_bar.add_cascade(label="Settings", menu=settings_menu)
-        settings_menu.add_command(label="User Settings", command=self.show_user_settings)
-
         # Help Menu
         help_menu = tk.Menu(menu_bar, tearoff=0)
         menu_bar.add_cascade(label="Help", menu=help_menu)
         help_menu.add_command(label="Documentation", command=self.open_documentation)
 
-    def add_code_tab(self):
-        # Text Editor
-        text_editor = scrolledtext.ScrolledText(self.notebook, wrap="word", undo=True, font=("Courier New", int(self.font_size.get())))
-        text_editor.pack(expand="yes", fill="both")
-        self.notebook.add(text_editor, text=f"Untitled {self.notebook.index(tk.END)}")
-
-        # Console
-        console = scrolledtext.ScrolledText(self.notebook, wrap="word", height=10, state=tk.DISABLED, font=("Courier New", int(self.font_size.get())))
-        console.pack(expand="yes", fill="both")
-        console.insert(tk.END, f"Python Console {self.notebook.index(tk.END)}\n")
-        self.notebook.add(console, text="Console")
+    def new_file(self):
+        self.text.delete(1.0, tk.END)
 
     def open_file(self):
         file_path = filedialog.askopenfilename(defaultextension=".txt",
@@ -90,39 +59,32 @@ class SimpleIDE:
         if file_path:
             with open(file_path, "r") as file:
                 content = file.read()
-                text_editor = self.notebook.select().children["!scrolledtext"]
-                text_editor.delete(1.0, tk.END)
-                text_editor.insert(tk.END, content)
+                self.text.delete(1.0, tk.END)
+                self.text.insert(tk.END, content)
 
     def save_file(self):
-        text_editor = self.notebook.select().children["!scrolledtext"]
         # If the file is already saved, overwrite it
-        if hasattr(text_editor, "file_path"):
-            with open(text_editor.file_path, "w") as file:
-                file.write(text_editor.get(1.0, tk.END))
+        if hasattr(self, "file_path"):
+            with open(self.file_path, "w") as file:
+                file.write(self.text.get(1.0, tk.END))
         # If the file is not saved yet, use Save As
         else:
             self.save_as_file()
 
     def save_as_file(self):
-        text_editor = self.notebook.select().children["!scrolledtext"]
         file_path = filedialog.asksaveasfilename(defaultextension=".txt",
                                                    filetypes=[("Text Files", "*.txt"), ("All Files", "*.*")])
         if file_path:
             with open(file_path, "w") as file:
-                file.write(text_editor.get(1.0, tk.END))
-                text_editor.file_path = file_path
-                # Update the tab name with the file name
-                self.notebook.tab(self.notebook.select(), text=file_path)
+                file.write(self.text.get(1.0, tk.END))
+                self.file_path = file_path
 
     def run_code(self):
-        text_editor = self.notebook.select().children["!scrolledtext"]
-        code = text_editor.get(1.0, tk.END)
-        console = self.notebook.select().children["!scrolledtext"]
+        code = self.text.get(1.0, tk.END)
         result = self.run_python_code(code)
-        console.config(state=tk.NORMAL)
-        console.insert(tk.END, f"\nOutput:\n{result}\n")
-        console.config(state=tk.DISABLED)
+        self.console.config(state=tk.NORMAL)
+        self.console.insert(tk.END, f"\nOutput:\n{result}\n")
+        self.console.config(state=tk.DISABLED)
 
     def run_python_code(self, code):
         try:
@@ -134,44 +96,12 @@ class SimpleIDE:
     def open_documentation(self):
         webbrowser.open("https://docs.python.org/3/")
 
-    def show_update_log_notification(self):
-        messagebox.showinfo("Update Log", self.update_log_text)
-
-    def clear_console(self):
-        console = self.notebook.select().children["!scrolledtext"]
-        console.config(state=tk.NORMAL)
-        console.delete(1.0, tk.END)
-        console.insert(tk.END, f"Python Console {self.notebook.index(tk.END)}\n")
-        console.config(state=tk.DISABLED)
-
-    def toggle_console(self):
-        console = self.notebook.select().children["!scrolledtext"]
-        current_state = console.cget("state")
-        new_state = tk.NORMAL if current_state == tk.DISABLED else tk.DISABLED
-        console.config(state=new_state)
-
-    def show_user_settings(self):
-        font_size = simpledialog.askinteger("User Settings", "Enter font size:", parent=self.root, initialvalue=int(self.font_size.get()))
-        if font_size is not None:
-            self.font_size.set(str(font_size))
-            self.apply_user_settings()
-
-    def apply_user_settings(self):
-        for tab_id in self.notebook.tabs():
-            tab = self.notebook.nametowidget(tab_id)
-            text_editor = tab.children["!scrolledtext"]
-            console = self.notebook.tab(tab, "text") == "Console" and tab.children["!scrolledtext"]
-            text_editor.configure(font=("Courier New", int(self.font_size.get())))
-            if console:
-                console.configure(font=("Courier New", int(self.font_size.get())))
-
-    def undo(self):
-        text_editor = self.notebook.select().children["!scrolledtext"]
-        text_editor.event_generate("<<Undo>>")
-
-    def redo(self):
-        text_editor = self.notebook.select().children["!scrolledtext"]
-        text_editor.event_generate("<<Redo>>")
+    def show_update_log_popup(self):
+        update_log_popup = tk.Toplevel(self.root)
+        update_log_popup.title("Update Log")
+        update_log_text = tk.Text(update_log_popup, wrap="word", state=tk.DISABLED)
+        update_log_text.pack(expand="yes", fill="both")
+        update_log_text.insert(tk.END, self.update_log_text)
 
 if __name__ == "__main__":
     root = tk.Tk()
